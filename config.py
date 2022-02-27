0962b794441055f6c6a07bb83ca919aea8ab95dc
gbimsath <91552385+gbimsath@users.noreply.github.com>
2022-02-27 16:16:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@
 import os
 
 class Config(object):
-    MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     CAPTION = os.environ.get("CAPTION", "")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
-    URL_LINK = os.environ.get("LINK", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    GROUP_LINK = os.environ.get("GROUP_LINK", "https://t.me/+rWHmdVt3NythYzI1")
+    CHANNEL_LINK = os.eviron.get("CHANNEL_LINK")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "https://t.me/MOHLK_MG_UPDATES")
